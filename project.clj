7fc3fe747aadfe08ff1e8217f2ced113e46ed1ac
Tyler Hobbs <tyler@tylerlhobbs.com>
2019-04-19 15:44:25 -0500

    Update deps, change group, support clj 1.10

--- project.clj
+++ project.clj
@@ -1,18 +1,14 @@
-(defproject io.aviso/logging "0.3.1"
+(defproject travelboss/logging "0.3.1"
   :description "Clojure logging with Logback and SLF4J plus request correlation across servers."
-  :url "https://github.com/AvisoNovate/logging"
+  :url "https://github.com/travelboss/logging"
   :license {:name "Apache Sofware License 2.0"
             :url  "http://www.apache.org/licenses/LICENSE-2.0.html"}
 
-  :jvm-opts ^:replace ["-Xmx1G" "-Xms1G" "-XX:+UseG1GC" "-XX:-OmitStackTraceInFastThrow"]
-
-  :javac-options ["-target" "1.7" "-source" "1.7"]
-
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.slf4j/slf4j-api "1.7.25"]
                  [ch.qos.logback/logback-classic "1.2.3"]
-                 [org.clojure/tools.logging "0.4.0"]
-                 [io.aviso/pretty "0.1.34"]
+                 [org.clojure/tools.logging "0.4.1"]
+                 [io.aviso/pretty "0.1.36"]
                  [org.slf4j/jcl-over-slf4j "1.7.25"]]
 
   :profiles
